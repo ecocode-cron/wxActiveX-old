fcac719319ad42a977252d0a628bf3ee85f8209f
mdootson <mdootson@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-11-13 02:52:13 +0000

    wxActiveX - SVN id update

--- lib/Wx/DemoModules/wxActiveX.pm
+++ lib/Wx/DemoModules/wxActiveX.pm
@@ -4,7 +4,7 @@
 ## Author:      Mark Dootson
 ## Modified by:
 ## Created:     13/11/2007
-## RCS-ID:      $Id:$
+## SVN-ID:      $Id:$
 ## Copyright:   (c) 2002 2007 Graciliano M. P. & Mark Dootson
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
