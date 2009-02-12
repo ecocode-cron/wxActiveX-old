c233f6fe834b2d393a585ed591f24b066ad81d0b
mdootson <mdootson@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2009-02-12 16:51:45 +0000

    Wx::ActiveX - accommodate changes in helpers.h

--- cpp/PlActiveX.h
+++ cpp/PlActiveX.h
@@ -11,25 +11,21 @@
 class wxPliActiveX:public wxActiveX
 {
     public:
-        
+
         static wxPliClassInfo ms_classInfo;
         virtual wxClassInfo *GetClassInfo() const { return &ms_classInfo; }
         wxPliVirtualCallback m_callback;
         wxPliActiveX( const char* package, wxWindow* _arg1, const wxString& _arg2, wxWindowID _arg3,
             const wxPoint& _arg4, const wxSize& _arg5, long _arg6, const wxString& _arg7)
         : wxActiveX( _arg1, _arg2, _arg3, _arg4, _arg5, _arg6, _arg7 ), m_callback( "Wx::ActiveX" )
-        {   
+        {
             m_callback.SetSelf( wxPli_make_object( this, package ), true );
         }
 
 };
 
-wxPliSelfRef* wxPliGetSelfForwxPliActiveX(wxObject* object)
-        { return &((wxPliActiveX *)object)->m_callback; }
-        
-wxPliClassInfo wxPliActiveX::ms_classInfo((wxChar *) wxT("wxPliActiveX"), 
-        &wxActiveX::ms_classInfo, NULL, (int) sizeof(wxPliActiveX),
-        (wxPliGetCallbackObjectFn) wxPliGetSelfForwxPliActiveX);
+WXPLI_IMPLEMENT_DYNAMIC_CLASS( wxPliActiveX,
+                               wxActiveX );
 
 
 class wxPliIEHtmlWin:public wxIEHtmlWin
@@ -41,19 +37,14 @@ class wxPliIEHtmlWin:public wxIEHtmlWin
         wxPliIEHtmlWin( const char* package, wxWindow* _arg1, wxWindowID _arg2,
             const wxPoint& _arg3, const wxSize& _arg4, long _arg5, const wxString& _arg6)
         : wxIEHtmlWin( _arg1, _arg2, _arg3, _arg4, _arg5, _arg6 ), m_callback( "Wx::IEHtmlWin" )
-        {   
+        {
             m_callback.SetSelf( wxPli_make_object( this, package ), true );
         }
 
 };
 
-wxPliSelfRef* wxPliGetSelfForwxPliIEHtmlWin(wxObject* object)
-        { return &((wxPliIEHtmlWin *)object)->m_callback; }
-        
-wxPliClassInfo wxPliIEHtmlWin::ms_classInfo((wxChar *) wxT("wxPliIEHtmlWin"), 
-        &wxIEHtmlWin::ms_classInfo, NULL, (int) sizeof(wxPliIEHtmlWin),
-        (wxPliGetCallbackObjectFn) wxPliGetSelfForwxPliIEHtmlWin);
-
+WXPLI_IMPLEMENT_DYNAMIC_CLASS( wxPliIEHtmlWin,
+                               wxIEHtmlWin );
 
 
 class wxPliMozillaHtmlWin:public wxMozillaHtmlWin
@@ -65,19 +56,14 @@ class wxPliMozillaHtmlWin:public wxMozillaHtmlWin
         wxPliMozillaHtmlWin( const char* package, wxWindow* _arg1, wxWindowID _arg2,
             const wxPoint& _arg3, const wxSize& _arg4, long _arg5, const wxString& _arg6)
         : wxMozillaHtmlWin( _arg1, _arg2, _arg3, _arg4, _arg5, _arg6 ), m_callback( "Wx::MozillaHtmlWin" )
-        {   
+        {
             m_callback.SetSelf( wxPli_make_object( this, package ), true );
         }
 
 };
 
-wxPliSelfRef* wxPliGetSelfForwxPliMozillaHtmlWin(wxObject* object)
-        { return &((wxPliMozillaHtmlWin *)object)->m_callback; }
-        
-wxPliClassInfo wxPliMozillaHtmlWin::ms_classInfo((wxChar *) wxT("wxPliMozillaHtmlWin"), 
-        &wxMozillaHtmlWin::ms_classInfo, NULL, (int) sizeof(wxPliMozillaHtmlWin),
-        (wxPliGetCallbackObjectFn) wxPliGetSelfForwxPliMozillaHtmlWin);
-
+WXPLI_IMPLEMENT_DYNAMIC_CLASS( wxPliMozillaHtmlWin,
+                               wxMozillaHtmlWin );
 
 // Local variables: //
 // mode: c++ //
